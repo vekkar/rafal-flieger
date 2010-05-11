8fed9b90a9e782225eca7198a467b5fd4b616698
rafal.flieger <rafal.flieger@bd1ee05c-a138-11de-86bd-4589e38204d3>
2010-05-11 16:13:43 +0000

    xxx

--- trunk/refactoring1/src/Classifier/defaultFileProcessor.java
+++ trunk/refactoring1/src/Classifier/defaultFileProcessor.java
@@ -8,46 +8,48 @@ import java.util.List;
 import java.util.StringTokenizer;
 import java.util.concurrent.ArrayBlockingQueue;
 
-public class defaultFileProcessor implements IFileProcessor
-{
-	File inputFile= null;
-	List<weather> data= new ArrayList<weather>();
-	private String localeDelim=",";
-	
+public class defaultFileProcessor implements IFileProcessor {
+	File inputFile = null;
+	List<weather> data = new ArrayList<weather>();
+	private String localeDelim = ",";
+
 	@Override
-	public List<weather> readFile()
-	{
+	public List<weather> readFile() {
 		StringTokenizer st = null;
-		String line=null;
-		
-		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-		while ((line = reader.readLine()) != null)
-		{
-			st = new StringTokenizer(line, localeDelim);
-			String aura = st.nextToken();
-			if (aura.equals("Sunny") || aura.equals("sunny"))
-			{
-				contents.append(line);
-				contents.append(",");
-				contents.append("great");
-				contents.append("/n");
-			}// heare handle another types of wheather;
+		String line = null;
+		try {
+			BufferedReader reader = new BufferedReader(
+					new FileReader(inputFile));
+			while ((line = reader.readLine()) != null) {
+				st = new StringTokenizer(line, localeDelim);
+				weather item = new weather();
+
+			}
+		} catch (Exception e) {
+
 		}
 		return null;
 	}
 
 	@Override
-	public void setFile(File file)
-	{
-		this.inputFile=file;		
+	public void setFile(File file) {
+		this.inputFile = file;
 	}
 
 	@Override
-	public List<weather> readFile(File file)
-	{
+	public List<weather> readFile(File file) {
 		this.setFile(file);
-		
+
 		return null;
-	} 
+	}
+
+	private weather createWeather(StringTokenizer st) {
+		weather item = new weather();
+		while(st.hasMoreElements())
+		{
+			item
+		}
+		return null;
+	}
 
 }
