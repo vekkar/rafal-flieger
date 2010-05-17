84fc332e317bc43e9a6b4a5eacb1446a0cd7c6d3
rafal.flieger <rafal.flieger@bd1ee05c-a138-11de-86bd-4589e38204d3>
2010-05-17 12:07:42 +0000

    cnd, prawie gotowe

--- trunk/refactoring1/src/Classifier/classifier.java
+++ trunk/refactoring1/src/Classifier/classifier.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class classifier
@@ -17,26 +18,11 @@ public class classifier
 
 		File inputFile = new File(inputFileName);
 		File outputFile = new File(outputFileName);
-
-		BufferedReader reader = null;
-		BufferedWriter writer = null;
 		
-		StringBuilder contents = new StringBuilder();
-
 		try
 		{
-			//what if file does not exist?
-			reader = new BufferedReader(new FileReader(inputFile));
-			writer = new BufferedWriter(new FileWriter(outputFile));
-			try
-			{
-				
-			} finally
-			{
-				reader.close();
-				writer.close();
-			}
-
+			IFileProcessor processor= new defaultFileProcessor();
+			List<weather> data= processor.readFile(inputFile);
 		} catch (Exception e)
 		{
 			e.printStackTrace();
--- trunk/refactoring1/src/Classifier/defaultFileProcessor.java
+++ trunk/refactoring1/src/Classifier/defaultFileProcessor.java
@@ -2,54 +2,67 @@ package Classifier;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 import java.util.concurrent.ArrayBlockingQueue;
 
-public class defaultFileProcessor implements IFileProcessor {
+public class defaultFileProcessor implements IFileProcessor
+{
 	File inputFile = null;
 	List<weather> data = new ArrayList<weather>();
 	private String localeDelim = ",";
 
 	@Override
-	public List<weather> readFile() {
+	public List<weather> readFile() throws Exception
+	{
 		StringTokenizer st = null;
 		String line = null;
-		try {
-			BufferedReader reader = new BufferedReader(
-					new FileReader(inputFile));
-			while ((line = reader.readLine()) != null) {
+		BufferedReader reader = null;
+		try
+		{
+			reader = new BufferedReader(new FileReader(inputFile));
+			while ((line = reader.readLine()) != null)
+			{
 				st = new StringTokenizer(line, localeDelim);
-				weather item = new weather();
-
+				weather newItem = this.createWeather(st);
+				newItem.doVerdict();
+				data.add(newItem);
 			}
-		} catch (Exception e) {
-
+		} finally
+		{
+			reader.close();
 		}
-		return null;
+		return data;
 	}
 
 	@Override
-	public void setFile(File file) {
+	public void setFile(File file)
+	{
 		this.inputFile = file;
 	}
 
 	@Override
-	public List<weather> readFile(File file) {
+	public List<weather> readFile(File file) throws Exception
+	{
 		this.setFile(file);
-
+		this.readFile();
 		return null;
 	}
 
-	private weather createWeather(StringTokenizer st) {
-		weather item = new weather();
-		while(st.hasMoreElements())
+	private weather createWeather(StringTokenizer st) throws Exception
+	{
+		// assume items in record have propper sequence
+		// attributes = { aura, temperature, humidity, wind }.
+		List<String> attributes = new ArrayList<String>();
+		while (st.hasMoreElements())
 		{
-			item
+			attributes.add(st.nextToken());
 		}
-		return null;
+		return new weather(attributes);
 	}
 
 }
