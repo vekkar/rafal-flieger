5369943e92e76b54bf01c656a48e7713ddd0ab21
rafal.flieger <rafal.flieger@bd1ee05c-a138-11de-86bd-4589e38204d3>
2010-05-17 12:07:42 +0000

    cnd, prawie gotowe

--- refactoring1/src/Classifier/IFileProcessor.java
+++ refactoring1/src/Classifier/IFileProcessor.java
@@ -1,11 +1,12 @@
 package Classifier;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 public interface IFileProcessor
 {
 	public void setFile(File file);
-	public List<weather> readFile();
-	public List<weather> readFile(File file);
+	public List<weather> readFile() throws Exception;
+	public List<weather> readFile(File file) throws Exception;
 }
